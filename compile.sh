c881ad3172bfbc18d3583b7b2ebeaff5388c651e
jpleitao <jocaleitao93@gmail.com>
2014-05-29 08:42:09 +0100

    Changed the compile and zip script to the new file names, and updated the file names and .gitignore file

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-lex ijparser.l && yacc --defines=y.tab.h ijparser.y && gcc -g -o ijparser *.c -ll -ly
+lex ijcompiler.l && yacc --defines=y.tab.h ijcompiler.y && gcc -g -o ijcompiler *.c -ll -ly
--- zip.sh
+++ zip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zip -r ijparser.zip ijparser.l ijparser.y node_t.c node_t.h sym_t.c sym_t.h
+zip -r ijcompiler.zip ijcompiler.l ijcompiler.y node_t.c node_t.h sym_t.c sym_t.h llvm_ir.c llvm_ir.h
