ba3897e2eaf5ba064abaa475174b759cc236d168
jpleitao <jocaleitao93@gmail.com>
2014-05-31 00:20:42 +0100

    Added comments to the example file about the initialization of a global int array in llvm

--- LLVM_Drafts/main_global_array_int_malloc.s
+++ LLVM_Drafts/main_global_array_int_malloc.s
@@ -3,13 +3,13 @@
 @array = common global %struct.IntArray zeroinitializer, align 8
 
 define i32 @main() nounwind uwtable {
-  store i32 40, i32* getelementptr inbounds (%struct.IntArray* @array, i32 0, i32 0), align 4
-  %1 = load i32* getelementptr inbounds (%struct.IntArray* @array, i32 0, i32 0), align 4
-  %2 = sext i32 %1 to i64
-  %3 = mul i64 %2, 4
-  %4 = call noalias i8* @malloc(i64 %3) nounwind
-  %5 = bitcast i8* %4 to i32*
-  store i32* %4, i32** getelementptr inbounds (%struct.IntArray* @array, i32 0, i32 1), align 8
+  store i32 40, i32* getelementptr inbounds (%struct.IntArray* @array, i32 0, i32 0), align 4 ;Guarda o tamanho do array
+  %1 = load i32* getelementptr inbounds (%struct.IntArray* @array, i32 0, i32 0), align 4 ; Vai buscar ponteiro para array.size
+  %2 = sext i32 %1 to i64 ;COnverte para 64 bits - Nao vamos usar
+  %3 = mul i64 %2, 4 ;Multiplica o tamanho do array por 4, porque e inteiro
+  %4 = call noalias i8* @malloc(i64 %3) nounwind ;Chama o malloc
+  %5 = bitcast i8* %4 to i32* ;Muda o i8* devolvido pelo malloc para i32*
+  store i32* %4, i32** getelementptr inbounds (%struct.IntArray* @array, i32 0, i32 1), align 8 ;Guarda o array na estrutura
   ret i32 0
 }
 
