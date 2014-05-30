5a387875a1ce7b714305fd11ea4421a3fa1592bf
jpleitao <jocaleitao93@gmail.com>
2014-05-31 00:29:03 +0100

    Finished documenting all the examples

--- LLVM_Drafts/main_global_array_bool_malloc.s
+++ LLVM_Drafts/main_global_array_bool_malloc.s
@@ -3,12 +3,12 @@
 @array = global %struct.IntArray zeroinitializer, align 8
 
 define i32 @main() nounwind uwtable {
-  store i32 40, i32* getelementptr inbounds (%struct.IntArray* @array, i32 0, i32 0), align 4
-  %1 = load i32* getelementptr inbounds (%struct.IntArray* @array, i32 0, i32 0), align 4
-  %2 = sext i32 %1 to i64
-  %3 = mul i64 %2, 4
+  store i32 40, i32* getelementptr inbounds (%struct.IntArray* @array, i32 0, i32 0), align 4 ;Guardar o tamanho do array
+  %1 = load i32* getelementptr inbounds (%struct.IntArray* @array, i32 0, i32 0), align 4 ;Carregar o tamanho do array
+  %2 = sext i32 %1 to i64 ;Nao vamos usar
+  %3 = mul i64 %2, 4 ;Obter tamanho final do array
   %4 = call noalias i8* @malloc(i64 %3) nounwind
-  store i8* %4, i8** getelementptr inbounds (%struct.IntArray* @array, i32 0, i32 1), align 8
+  store i8* %4, i8** getelementptr inbounds (%struct.IntArray* @array, i32 0, i32 1), align 8 ;Armazenar na estrutura o ponteiro para o array
   ret i32 0
 }
 
